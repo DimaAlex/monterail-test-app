ccca98a74e7ec8fd0f30412c7568076e3a42e3de
Dzmitry Aliakseichyk <dima_drakon@inbox.ru>
2020-12-13 01:31:58 +0100

    Enabling annotate gem

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: events
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Event < ApplicationRecord
   has_many :sectors
 end
